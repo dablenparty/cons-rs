64b842db9ed355cf746dc11244da481e358e5f0f
dablenparty <hdavenport8902@gmail.com>
2023-10-31 14:26:19 -0400

    Update syntax to allow iter expressions

--- src/lib.rs
+++ src/lib.rs
@@ -11,19 +11,18 @@
 /// ```rust
 /// # use cons::cons;
 /// let v = [1, 2, 3];
-/// cons!(v => x::xs);
+/// cons!(v as x::xs);
 /// assert_eq!(x, 1);
 /// assert_eq!(xs, vec![2, 3]);
 /// ```
 ///
-/// The syntax is not exactly the same as SML's. The reason for this is
-/// explained further in the _Limitations_ section of the [crate-level documentation](crate).
-/// As a result of this, you can create/use iterators directly in the
-/// macro invocation:
+/// You can even use iterators directly in the macro invocation;
+/// however, in order to match the syntax to SML as closely as
+/// possible, you must wrap the iterator in parentheses:
 ///
 /// ```rust
 /// # use cons::cons;
-/// cons!(1..=3 => x::xs);
+/// cons!((1..=3) as x::xs);
 /// assert_eq!(x, 1);
 /// assert_eq!(xs, vec![2, 3]);
 /// ```
@@ -33,7 +32,7 @@
 /// ```rust
 /// # use cons::cons;
 /// let v = [1, 2, 3, 4, 5];
-/// cons!(v => a::b::c::d::e::nil);
+/// cons!(v as a::b::c::d::e::nil);
 /// // ...
 /// # assert_eq!(a, 1);
 /// # assert_eq!(b, 2);
@@ -58,7 +57,7 @@
 ///    String::from("the"),
 ///    String::from("world"),
 /// ];
-/// cons!(v => x::y::zs);
+/// cons!(v as x::y::zs);
 /// assert_eq!(x, "hello");
 /// assert_eq!(y, "to");
 /// assert_eq!(zs, vec!["the", "world"]);
@@ -75,7 +74,7 @@
 /// ```rust
 /// # use cons::cons;
 /// let tuples = [(1, 2), (3, 4), (5, 6)];
-/// cons!(tuples => ((x, y))::zs);
+/// cons!(tuples as ((x, y))::zs);
 /// assert_eq!(x, 1);
 /// assert_eq!(y, 2);
 /// assert_eq!(zs, vec![(3, 4), (5, 6)]);
@@ -84,7 +83,7 @@
 /// struct Point(i32, i32);
 ///
 /// let points = [Point(1, 2), Point(3, 4), Point(5, 6)];
-/// cons!(points => (Point(x, y))::zs);
+/// cons!(points as (Point(x, y))::zs);
 /// assert_eq!(x, 1);
 /// assert_eq!(y, 2);
 /// assert_eq!(zs, vec![Point(3, 4), Point(5, 6)]);
@@ -99,7 +98,7 @@
 /// ```rust,should_panic
 /// # use cons::cons;
 /// let v = [1];
-/// cons!(v => x::y::zs); // panics, iter not long enough
+/// cons!(v as x::y::zs); // panics, iter not long enough
 /// ```
 ///
 /// If `nil` is at the end of the pattern and there are still elements
@@ -109,14 +108,20 @@
 /// ```rust,should_panic
 /// # use cons::cons;
 /// let v = [1, 2];
-/// cons!(v => x::nil); // panics, iter still has [2]
+/// cons!(v as x::nil); // panics, iter still has an element
 /// ```
 #[macro_export]
 macro_rules! cons {
-    ($iter:expr => $hd:ident :: nil) => {
-        $crate::cons!($iter => ($hd)::nil);
+    ($iter:ident as $($rest:tt)+) => {
+        $crate::cons!(@__ $iter => $($rest)+);
     };
-    ($iter:expr => ($hd:pat) :: nil) => {
+    (($iter:expr) as $($rest:tt)+) => {
+        $crate::cons!(@__ $iter => $($rest)+);
+    };
+    (@__ $iter:expr => $hd:ident :: nil) => {
+        $crate::cons!(@__ $iter => ($hd)::nil);
+    };
+    (@__ $iter:expr => ($hd:pat) :: nil) => {
         let mut iter = $iter.into_iter();
         let $hd = iter.next().unwrap_or_else(|| {
             panic!("Iterator exhausted before reaching variable {}", stringify!($hd));
@@ -126,19 +131,19 @@ macro_rules! cons {
             assert_eq!(rest, 0, "Found `nil` in cons but iterator is not empty ({rest} elements left)\nConsider removing `::nil`");
         }
     };
-    ($iter:expr => $hd:ident) => {
+    (@__ $iter:expr => $hd:ident) => {
         let iter = $iter.into_iter();
         let $hd = iter.collect::<Vec<_>>();
     };
-    ($iter:expr => $hd:ident :: $($rest:tt)+) => {
-        $crate::cons!($iter => ($hd) :: $($rest)+);
+    (@__ $iter:expr => $hd:ident :: $($rest:tt)+) => {
+        $crate::cons!(@__ $iter => ($hd) :: $($rest)+);
     };
-    ($iter:expr => ($hd:pat) :: $($rest:tt)+) => {
+    (@__ $iter:expr => ($hd:pat) :: $($rest:tt)+) => {
         let mut iter = $iter.into_iter();
         let $hd = iter.next().unwrap_or_else(|| {
             panic!("Iterator exhausted before reaching variable {}", stringify!($hd));
         });
-        $crate::cons!(iter => $($rest)+);
+        $crate::cons!(@__ iter => $($rest)+);
     };
 }
 
@@ -150,7 +155,7 @@ mod tests {
     fn test_get_head() {
         let v = [1, 2, 3];
         let expected = (1, vec![2, 3]);
-        cons!(v => x::xs);
+        cons!(v as x::xs);
         assert_eq!(x, expected.0);
         assert_eq!(xs, expected.1);
     }
@@ -159,19 +164,20 @@ mod tests {
     fn test_triple_cons() {
         let v = [1, 2, 3, 4, 5];
         let expected = (1, 2, vec![3, 4, 5]);
-        cons!(v => x::y::zs);
+        cons!(v as x::y::zs);
         assert_eq!(x, expected.0);
         assert_eq!(y, expected.1);
         assert_eq!(zs, expected.2);
     }
 
     #[test]
-    fn test_destructure_inline() {
-        let v = [(1, 2), (3, 4)];
-        cons!(v => ((x, y))::zs);
-        assert_eq!(x, 1);
-        assert_eq!(y, 2);
-        assert_eq!(zs, vec![(3, 4)]);
+    fn test_destructure_in_middle() {
+        let v = [(1, 2), (3, 4), (5, 6)];
+        cons!(v as w::((x, y))::zs);
+        assert_eq!(w, (1, 2));
+        assert_eq!(x, 3);
+        assert_eq!(y, 4);
+        assert_eq!(zs, vec![(5, 6)]);
     }
 
     #[test]
@@ -179,7 +185,7 @@ mod tests {
     #[allow(unused_variables)]
     fn test_iterator_too_short() {
         let v = [1];
-        cons!(v => x::y::zs);
+        cons!(v as x::y::zs);
     }
 
     #[test]
@@ -189,6 +195,6 @@ mod tests {
     #[allow(unused_variables)]
     fn test_iterator_too_long() {
         let v = [1, 2];
-        cons!(v => x::nil);
+        cons!(v as x::nil);
     }
 }
